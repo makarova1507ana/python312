5a45c2bfdd313ca3567a5f7bf781b72942af8a1f
Anastasiia <makarova1507nastya+1@gmail.com>
2024-03-18 19:21:38 +0100

    add views

--- pets/urls.py
+++ pets/urls.py
@@ -3,5 +3,12 @@ from django.urls import path, re_path
 import pets.views as pets
 
 urlpatterns = [
-    path('pets/', pets.index), # http://127.0.0.1:8000/pets/
+    path('', pets.index), # http://127.0.0.1:8000/pets/
+    # path('cats/', pets.cats),  # http://127.0.0.1:8000/pets/cats/
+    # path('dogs/', pets.dogs),  # http://127.0.0.1:8000/pets/dogs/
+    path('petGET/', pets.petGET),  # http://127.0.0.1:8000/pets/petGET/
+    path('<slug:pet_slug>/', pets.pet), # http://127.0.0.1:8000/pets/{pet_slug}/
+
+
+    path('categories/<int:categorie_id>/', pets.categories), # http://127.0.0.1:8000/pets/categories/{int}/
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,8 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("ура страница появилась")
+    return HttpResponse("<h2>Главная <a href='http://127.0.0.1:8000/pets/cats/'>Коты</a> Собаки</h2>")
 
-def page2(request):
-    return HttpResponse("ура страница появилась")
+# def cats(request):
+#     return HttpResponse("<h2>Коты</h2>")
+#
+# def dogs(request):
+#     return HttpResponse("<h2>Собаки</h2>")
+#
+def pet(request, pet_slug):
+    # будем делать обращение к БД " существует ли pet_slug ?"
+    if pet_slug in ['cats', 'dogs']:
+        return HttpResponse(f"<h2>{pet_slug}</h2>")
+    return HttpResponseNotFound(f"<h2>ОШИБКА!!! Нет такой страницы!</h2><img src='https://i.pinimg.com/736x/c9/e3/eb/c9e3eb487b0deb3f50501c196e332b58.jpg'>") # будет отправлен статус код 404
+
+
+def petGET(request):
+    title = request.GET.get('title')# лучше примернять когда есть форма
+    return HttpResponse(f"<h2>{title}</h2>")
+
+
+
+def categories(request, categorie_id):
+    return HttpResponse(f"<h2>Категории</h2><p>id:{ categorie_id }</p>")
