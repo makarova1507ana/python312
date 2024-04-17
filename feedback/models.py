3dbc0a345a79f95cf064b2d243d5094155fa9259
Anastasiia <makarova1507nastya+1@gmail.com>
2024-04-17 20:36:51 +0200

    Добавили пример с валидацией, добавили загрузку фото на сервер

--- feedback/models.py
+++ feedback/models.py
@@ -4,5 +4,8 @@ from django.db import models
 
 class Feedback(models.Model):
     text = models.CharField(max_length=30)
+    rating = models.IntegerField(default=0)# оценка
+    image = models.ImageField(upload_to='feedback_images/', blank=True, null=True)
+    # фото
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
--- feedback/views.py
+++ feedback/views.py
@@ -3,29 +3,39 @@ from django.shortcuts import render
 from feedback.forms import *
 import datetime
 
+
+
 def feedback(request):
+    # feedback получить все записи
+    results = Feedback.objects.all()
+
+    # передать в шаблон
+    data = {
+        "feedback": results,
+        'add_feedback_form': AddFeedbackForm()
+    }
 
     if request.method == 'POST':
         # ------------------- CREATE ---------------------#
        # text_feedback = request.POST.get('text')
        # Feedback.objects.create(text=text_feedback)
 
-        form = AddFeedbackForm(request.POST) # словарь со всеми параметрами запроса
-        form.save()
-        return HttpResponse(f"""
-        <h2>Ваш отзыв успешно отправлен</h2>
-        <a href=""><button>Вернуться на страницу с отзывами</button></a>
-""")
+        form = AddFeedbackForm(request.POST, request.FILES) # словарь со всеми параметрами запроса
+        if form.is_valid():
+            form.save()
+            return HttpResponse(f"""
+            <h2>Ваш отзыв успешно отправлен</h2>
+            <a href=""><button>Вернуться на страницу с отзывами</button></a>
+    """)
+
+        form.add_error(None, "Ошибка добавления данных")
+        data[ 'add_feedback_form'] = form
+        return render(request, 'feedback.html', data)
+
+
     elif request.method == 'GET':
         # ------------------- READ ---------------------#
-        # feedback получить все записи
-        results = Feedback.objects.all()
 
-        # передать в шаблон
-        data = {
-            "feedback": results,
-            'add_feedback_form': AddFeedbackForm()
-        }
 
         return render(request, 'feedback.html', data)
 
@@ -33,22 +43,32 @@ def feedback(request):
 
 def editFeedback(request, feedback_id):
     feedbackData = Feedback.objects.get(id=feedback_id)
+    # отобразить инфомарцию о текущем отзыве
+    data = {"id": feedback_id,
+            "text": feedbackData.text,
+            'add_feedback_form': AddFeedbackForm(),
+            "date_created": feedbackData.date_created}
     if request.method == "GET":
-        # отобразить инфомарцию о текущем отзыве
-        data = {"id": feedback_id,
-                "text": feedbackData.text,
-                'add_feedback_form': AddFeedbackForm(),
-                "date_created": feedbackData.date_created}
+
         # отобразить форму
         return render(request, 'editFeedback.html', data)
 
     # ------------------- UPDATE ---------------------#
     if request.method == 'POST':
         # обработать редактирование
-        feedbackData.text = request.POST.get('text')
-        feedbackData.date_updated = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        feedbackData.save()
-        return HttpResponseRedirect("../../")
+        form = AddFeedbackForm(request.POST)  # словарь со всеми параметрами запроса
+        if form.is_valid():
+            feedbackData.text = request.POST.get('text')# заменить на form , сейчас вместо редактирования создает новый отзыв
+            feedbackData.rating = request.POST.get('rating')#
+            feedbackData.date_updated = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+            feedbackData.save()
+            return HttpResponseRedirect("../../")
+
+        form.add_error(None, "Ошибка добавления данных")
+        data['add_feedback_form'] = form
+        return render(request, 'editFeedback.html', data)
+
+
 
 # удаление данных из бд
 def deleteFeedback(request, feedback_id):
